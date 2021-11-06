5f11fe94c8d9b3f2c3259a688b58ed5033eaa879
Jean Vides <developers@jean.host>
2021-11-06 12:44:43 -0500

    .

--- pgk/httpserver/routes.go
+++ pgk/httpserver/routes.go
@@ -6,6 +6,12 @@ import (
 
 func setHandlers() {
 	router.HandleFunc("/", routerHandler)
+	
+	router.HandleFunc("/jean", jeanHandler)
+	router.HandleFunc("/chat", chatHandler)
+	router.HandleFunc("/short", shortenerHandler)
+	router.HandleFunc("/shortener", shortenerHandler)
+
 	api.HandleFunc("/", apiHandler)
 	v1.HandleFunc("/", v1Handler)
 }
@@ -14,6 +20,18 @@ func routerHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("{\"services\": \"https://chat.jean.host https://short.jean.host \",\"domains\": \"jean.host jeanvides.me jeanservices.co cagaca.ga vides.tech jeann.studio elemental.ga\",\"contact\": \"contact@jean.host\"}"))
+}
+
+func jeanHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://jeanvides.me", http.StatusMovedPermanently)
+}
+
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://chat.jean.host", http.StatusMovedPermanently)
+}
+
+func shortenerHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://short.jean.host", http.StatusMovedPermanently)
 }
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
